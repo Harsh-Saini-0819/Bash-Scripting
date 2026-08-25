9717bb0d6729af506f21a9b0bd1ecc6ace512d49
Harsh-Saini-0819 <harshgithub0819@gmail.com>
2026-08-26 00:29:48 +0530

    code file added

---
+++ Archive_Large_Files/main.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Date of creation - Tue Aug 25 12:35:41 PM EDT 2026
+
+if [[ $# -lt 1 ]]
+then
+	echo "Directory path missing. Provide the path !!!"
+	echo "Usage : ${0} /path to directory"
+	exit 1
+fi
+
+TARGET_PATH="$1"
+RUN=0
+DEPTH=1
+
+#check  if the directory present or not
+
+if [[ ! -d "$TARGET_PATH" ]]
+then
+	echo "Directory not found : $TARGET_PATH"
+	exit 1
+fi
+
+#Create archive folder if not present
+if [[ ! -d "$TARGET_PATH/archive" ]]
+then
+	mkdir "$TARGET_PATH/archive"
+fi
+
+#find thr list of files larger than 10KB
+
+for i in `find "$TARGET_PATH" -maxdepth "$DEPTH" -type f -size +10k`
+do
+	if [[ $RUN -eq 0 ]]
+	then
+		echo "[$(date "+%Y-%m-%d %H:%M:%S")] archiving $i ==> $TARGET_PATH/archive"
+		gzip "$i" || exit 1
+		mv "$i.gz" "$TARGET_PATH/archive" || exit 1
+	fi	
+done
